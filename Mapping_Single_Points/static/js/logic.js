14c6db23d0a9a763eece7cd791c31f46dcacd7b7
Madhulina Bandyopadhyay <mbandyo@hotmail.com>
2022-10-16 13:06:05 -0500

    Code change in logic.

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -8,6 +8,9 @@ let map = L.map("mapid", {
     ],
     zoom: 4
   });
+  L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
