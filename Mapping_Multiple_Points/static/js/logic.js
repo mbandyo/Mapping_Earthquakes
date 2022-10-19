a7b8242aacbc74d531abab4875053ce688d81653
Madhulina Bandyopadhyay <mbandyo@hotmail.com>
2022-10-19 07:53:34 -0500

    Commiting new code.

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,16 +1,21 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-// Create the map object with a center and zoom level.
 let map = L.map("mapid", {
     center: [
       40.7, -94.5
     ],
     zoom: 4
   });
-  L.circle([34.0522, -118.2437], {
-    radius: 100
- }).addTo(map);
+  // Get data from cities.js
+let cityData = cities;
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {radius: city.population/100000})
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
